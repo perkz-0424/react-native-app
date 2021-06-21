1d36650a4e7dc1b62709fde3f8052c111e26f5ee
Hu Fei <71580261+perkz-0424@users.noreply.github.com>
2021-06-21 17:27:14 +0800

    14

---
+++ src/modules/HomePage/User/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const User = prop => {
+  return (
+    <View>
+      <Text>个人</Text>
+    </View>
+  );
+};
+
+export default User;
---
+++ src/modules/HomePage/Warning/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Warning = prop => {
+  return (
+    <View>
+      <Text>告警</Text>
+    </View>
+  );
+};
+
+export default Warning;
