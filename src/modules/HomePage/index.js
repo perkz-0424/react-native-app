5e374d8df48c3acc0fd7873fc80ebba689742356
Hu Fei <71580261+perkz-0424@users.noreply.github.com>
2021-06-21 23:16:04 +0800

    20

--- src/modules/HomePage/index.js
+++ src/modules/HomePage/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { View, Dimensions, StyleSheet, Image, Platform } from "react-native";
+import { View, Dimensions, StyleSheet, Image, Platform, TouchableOpacity, Text } from "react-native";
 import Title from "../../components/Title";
 import Warning from "./Warning";
 import Moint from "./Moint";
@@ -9,45 +9,71 @@ import Homework from "./Homework";
 import User from "./User";
 
 const Tab = createBottomTabNavigator();
-const HomePage = ({ navigation }) => {
+const HomePage = () => {
   const [title, set_title] = useState("告警列表");
-  const pressNavigationEvent = (e) => {
-    console.log(e);
+  const pressNavigationEvent = (name) => {
+    switch (name) {
+      case "Warning":
+        set_title("告警列表");
+        break;
+      case "Moint":
+        set_title("实时监控");
+        break;
+      case "Resours":
+        set_title("资源信息");
+        break;
+      case "Homework":
+        set_title("维护作业");
+        break;
+      case "User":
+        set_title("个人中心");
+        break;
+      default:
+        set_title("告警列表");
+    }
   };
-
-  useMemo(() => {
-
-  }, []);
-
-  useEffect(() => {
-    return () => {
-
-    };
-  }, []);
   return (
     <View
-      style={{ height: Platform.OS === "ios" ? Dimensions.get("window").height : Dimensions.get("window").height - 24.5 }}>
-      <Title
-        title={title}
-        onLeftPress={() => {}}
-        titleLeft={<View></View>}
-      />
+      style={{ height: Platform.OS === "ios" ? Dimensions.get("window").height : Dimensions.get("window").height - 25 }}>
+      {
+        title !== "个人中心" ? <Title
+          title={title}
+          onLeftPress={() => {}}
+          titleLeft={<View><Text>浙江</Text></View>}
+        /> : null
+      }
       <Tab.Navigator initialRouteName={"Warning"} lazy={true}>
         {
           bottomNav.map((item, index) => {
             return (
               <Tab.Screen
+                key={index}
                 name={item.name}
                 component={item.component}
                 options={{
-                  title: item.title,
-                  tabBarIcon: ({ color }) => (
-                    <Image
-                      style={{ ...styles.image, ...{ tintColor: color } }}
-                      source={item.icon}
-                    />
-                  ),
-                }} key={index}/>
+                  tabBarButton: (props) => {
+                    return (
+                      <TouchableOpacity {...props} onPress={() => {
+                        props.onPress();
+                        pressNavigationEvent(item.name);
+                      }}><Image style={{
+                        ...styles.image,
+                        ...{
+                          tintColor: props.accessibilityStates[0] === "selected" ? "#469af7" : "#9c9c9c"
+                        }
+                      }} source={item.icon}
+                      /><Text style={{
+                        fontSize: 9,
+                        marginTop: 8,
+                        fontWeight: "400",
+                        color: props.accessibilityStates[0] === "selected" ? "#469af7" : "#9c9c9c"
+                      }}>{item.title}
+                      </Text>
+                      </TouchableOpacity>
+                    );
+                  }
+                }}
+              />
             );
           })
         }
